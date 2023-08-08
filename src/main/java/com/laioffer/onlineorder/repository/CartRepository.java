4c24680d2b12b36f8d90a5a7c3aa54a8c1209b91
Yuxiang Skyler Han <105407516+pineandbamboo@users.noreply.github.com>
2023-08-07 23:05:21 -0400

    update

---
+++ src/main/java/com/laioffer/onlineorder/repository/CartRepository.java
@@ -0,0 +1,15 @@
+package com.laioffer.onlineorder.repository;
+
+import com.laioffer.onlineorder.entity.CartEntity;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface CartRepository extends ListCrudRepository<CartEntity, Long> {
+    // 确定data type：第一个：第二个：Primay Key是什么type的
+    CartEntity getByCustomerId(long customerId);
+    @Modifying
+    @Query("UPDATE carts SET total_price = :totalPrice WHERE id = :cartId")
+    void updateTotalPrice(Long cartId, Double totalPrice);
+}
